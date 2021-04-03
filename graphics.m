9dbf1042386d9f3ddcbba5caac88845971661131
mariarodriguez <mariarodriguez@marias-mbp.devices.brown.edu>
2021-04-03 11:52:05 -0400

    at this point, I got it to display the title of game :) (1 hour)

--- graphics.m
+++ graphics.m
@@ -1,5 +1,26 @@
-%background
+%for PTB to run this line is needed
+Screen('Preference','SkipSyncTests',1);
+ScreenTest;
+
+%set background to a gray screen
+screens = Screen('Screens');
+screenNumber = max(screens);
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+grey = white / 2;
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
+
+
+%display the name of game on top of the screen
+%%store pixels of screen
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+Screen('TextSize', window, 70);
+Screen('TextFont', window, 'Courier');
+DrawFormattedText(window, 'Mastermind',...
+'center', screenYpixels * 0.5, [0 0 1]);
+Screen('Flip', window);
+KbStrokeWait;
 
 %board game 
 
-%keys
+%keys that correspond to each color
