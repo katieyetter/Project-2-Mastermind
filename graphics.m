85e4aed73d9067b78831305e46d3f671a32daaac
mariarodriguez <mariarodriguez@marias-mbp.devices.brown.edu>
2021-04-13 11:28:51 -0400

    Made color circles and trial rectangles (1.5 hours)

--- graphics.m
+++ graphics.m
@@ -1,6 +1,10 @@
+% Clear the workspace and the screen
+sca;
+close all;
+clearvars;
+
 %for PTB to run this line is needed
-Screen('Preference','SkipSyncTests',1);
-ScreenTest;
+PsychDefaultSetup(2);
 
 %set background to a gray screen
 screens = Screen('Screens');
@@ -23,7 +27,7 @@ screenXpixels * 0.1, screenYpixels * 0.1, white);
 %how to play button and script
 %%script
 instructions = 'You have 10 opportunities \n to guess the code! \n After every try \n you will see a green dot \n if you guessed the \n correct position and color \n or a white dot if \n you guessed incorrectly';
-keylegend = '\n click y for a yellow circle \n click r for a red circle \n click b for a blue circle \n click g for a green circle';
+keylegend = '\n\n click y for a yellow circle \n click r for a red circle \n click b for a blue circle \n click g for a green circle';
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 Screen('TextSize', window, 30);
 Screen('TextFont', window);
@@ -34,14 +38,10 @@ screenXpixels * 0.1, screenYpixels * 0.3, white);
 
 %board game 
 %%rectangle outline
-% Get the centre coordinate of the window
 [xCenter, yCenter] = RectCenter(windowRect);
-% Make a base Rect of 200 by 200 pixels
 baseRect = [0 0 200 280];
-% Screen X positions of our three rectangles
 squareXpos = [screenXpixels * 0.5];
 numSqaures = length(squareXpos);
-% Make a multiplier to modulate the size of our squares
 sizeChanger = [3];
 % Make our rectangle coordinates
 Rect = nan(4, 3);
@@ -52,16 +52,27 @@ end
 % Draw the rect to the screen
 Screen('FillRect', window, black, Rect);
 
+%%trial rectangles
+eachrect = [0 0 150 75];
+ycor = 717;
+for i = 1:10 
+centeredRect = CenterRectOnPointd(eachrect, xCenter, ycor + 80);
+end 
+Screen('FillRect', window, white, centeredRect);
+
+
 
 %circles and keys that correspond to each color
-dotXpos = screenXpixels * 0.8;
-dotYpos = screenYpixels + 290;
-buttonSize = 50;
-Screen('DrawDots', window, [dotXpos + 50; dotYpos], buttonSize, [0 1 0]);
-Screen('DrawDots', window, [dotXpos + 100; dotYpos], buttonSize, [1 0 0]);
-Screen('DrawDots', window, [dotXpos + 150; dotYpos], buttonSize, [0 0 1]);
-Screen('DrawDots', window, [dotXpos + 175; dotYpos], buttonSize, [0 1 1]);
+%"full red" is [1 0 0]. "Full green" [0 1 0] and "full blue" [0 0 1]
+buttonSize = 70;
+Screen('DrawDots', window, [700; 1020], buttonSize, [1 0 0], [], 2);
+Screen('DrawDots', window, [880; 1020], buttonSize, [0 1 0], [], 2);
+Screen('DrawDots', window, [1060; 1020], buttonSize, [0 0 1], [], 2);
+Screen('DrawDots', window, [1240; 1020], buttonSize, [1 1 0], [], 2);
 
 Screen('Flip', window);
 
 %exit button 
+exrec = [0 0 150 75];
+sideRect = CenterRectOnPointd(exrec, 1300, 800);
+Screen('FillRect', window, white, sideRect);
