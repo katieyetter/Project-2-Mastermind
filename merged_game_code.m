ec9ad822b942bb42e87854a188c80e7602e91a8b
mde11 <mde11@10.38.28.98>
2021-04-18 14:25:37 -0400

    Switched to fprinf instead of disp, added option to check input colors, tested and debugged code; time = 2.5 hours.

--- merged_game_code.m
+++ merged_game_code.m
@@ -11,24 +11,31 @@ for value = 1:length(pattern)
   generatedColors(value) = setColors(pattern(value));
 end
 
+disp(generatedColors);
 numguesses = 1;
 code_guess = 0;
+checked = 0;
 
-disp('Welcome to Mastermind.');
-disp('I have chosen four colors.');
-disp('Guess the color and the position. You have ten tries.');
+fprintf('Welcome to Mastermind.\nI have chosen four colors.\nGuess the color and position.\nYou have 10 tries.\n\n');
 
 while (numguesses < 11 && ~code_guess)
-    disp(append('Round ',string(numguesses)));
-    disp('Please input your guess for the colors one at a time from left to right');
-    disp('Colors may appear more than once and your choices are:');
-    disp('r = red, b = blue, g = green, and y = yellow');
+    checked = 0;
+    
+    while ~checked
+    fprintf('Round %s \nPlease input your guess for the colors one at a time from left to right.\nColors may appear more than once and your choices are:\nr = red, b = blue, g = green, and y = yellow\n', string(numguesses));
 
     guessColors = strings(1,4); %input user guesses as strings
     guessColors(1) = input('Color 1: ','s');
     guessColors(2) = input('Color 2: ','s');
     guessColors(3) = input('Color 3: ','s');
     guessColors(4) = input('Color 4: ','s');
+    
+    fprintf('%s ',guessColors');
+    colorCheck = input('\nAre these the correct colors you want to guess? (y/n)', 's');
+        if strcmp(colorCheck,'y')
+            checked = 1;
+        end
+    end
     
     correctPosition = 0;
     greenDots = zeros(1,4);
@@ -60,25 +67,24 @@ while (numguesses < 11 && ~code_guess)
         end
     end
     
-    disp(append('Number of green dots (correct color and position) is ', string(correctPosition)));
-    disp(append('Number of red dots (correct color but not position) is ', string(correctColor)));
+    fprintf('Number of green dots (correct color and position) is %s \n', string(correctPosition));
+    fprintf('Number of red dots (correct color but not position) is %s \n', string(correctColor));
     
     if correctPosition == 4
         %if user guesses correctly
-        disp('You won!');
-        disp(append('It took you ',string(numguesses),' guesses.'));
+        fprintf('You won!\nIt took you %s guesses\n',string(numguesses));
         code_guess = 1;
         
         
     elseif numguesses == 10
         %if user fails to guess correctly after 10 rounds
-        disp('You lost')
-        disp('The correct sequence of colors is:')
-        disp(generatedColors);
+        fprintf('You lost\nThe correct sequence of colors is:\n')
+        fprintf('%s ',generatedColors);
+        %disp(generatedColors);
         code_guess = 1;
     end
     if code_guess == 1
-        play_again = input('Would you like to play again? (y/n):','s');
+        play_again = input('\nWould you like to play again? (y/n):','s');
         if strcmp(play_again,'y')
             code_guess = 0;
             numguesses = 0;
@@ -89,7 +95,7 @@ while (numguesses < 11 && ~code_guess)
                 generatedColors(value) = setColors(pattern(value));
             end
         else
-            disp('Thank you for playing Mastermind. Goodbye!');
+            fprintf('Thank you for playing Mastermind. Goodbye!\n');
         end
     end
     numguesses = numguesses + 1;
